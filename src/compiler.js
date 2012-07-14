6137b664cb75b484c921196145bad161435a4174
Piggysoft <piggysoft0@gmail.com>
2012-07-13 18:10:59 -0700

    Update docs.

--- src/compiler.js
+++ src/compiler.js
@@ -388,7 +388,7 @@
 
   Scope.prototype.getView = function getView(type) {
     return this.frame.getView(type);
-  };
+  }
 
   Scope.prototype.MALLOC = function MALLOC() {
     return this.frame.MALLOC();
@@ -404,18 +404,6 @@
 
   Scope.prototype.MEMSET = function MEMSET(size) {
     return this.frame.MEMSET(size);
-  };
-  
-  Scope.prototype.MEMCHECK_CALL_PUSH = function MEMCHECK_CALL_PUSH() {
-    return this.frame.MEMCHECK_CALL_PUSH();
-  };
-  
-  Scope.prototype.MEMCHECK_CALL_RESET = function MEMCHECK_CALL_RESET() {
-    return this.frame.MEMCHECK_CALL_RESET();
-  };
-  
-  Scope.prototype.MEMCHECK_CALL_POP = function MEMCHECK_CALL_POP() {
-    return this.frame.MEMCHECK_CALL_POP();
   };
 
   Scope.prototype.toString = function () {
@@ -481,18 +469,6 @@
     case 4: name = "memset4"; ty = memset4Ty; break;
     }
     return getCachedLocal(this, name, ty);
-  };
-  
-  Frame.prototype.MEMCHECK_CALL_PUSH = function MEMCHECK_CALL_PUSH() {
-    return getCachedLocal(this, "memcheck_call_push", "dyn");
-  };
-  
-  Frame.prototype.MEMCHECK_CALL_RESET = function MEMCHECK_CALL_RESET() {
-    return getCachedLocal(this, "memcheck_call_reset", "dyn");
-  };
-  
-  Frame.prototype.MEMCHECK_CALL_POP = function MEMCHECK_CALL_POP() {
-    return getCachedLocal(this, "memcheck_call_pop", "dyn");
   };
 
   Frame.prototype.getView = function getView(ty) {
@@ -975,7 +951,7 @@
     node.ty = ty;
     return node;
   }
-  
+
   Node.prototype.transform = T.makePass("transform", "transformNode");
 
   function compileList(list, o) {
@@ -989,7 +965,6 @@
     }
     return translist;
   }
-  
 
   TypeAliasDirective.prototype.transform = function () {
     return null;
@@ -1008,7 +983,6 @@
     o = extend(o);
     o.scope = this.frame;
 
-    
     assert(this.body instanceof BlockStatement);
     this.body.body = compileList(this.body.body, o);
 
@@ -1084,7 +1058,7 @@
 
     if (!this.init && ty && typeof ty.defaultValue !== "undefined") {
       this.init = new Literal(ty.defaultValue);
-   }
+    }
 
     if (this.init) {
       var a = (new AssignmentExpression(this.id, "=", this.init, this.init.loc)).transform(o);
@@ -1568,8 +1542,6 @@
         variables.push(v);
       }
     }
-    
-      
 
     // Do this after the SP calculation since it might bring in U4. Since this
     // is after, we need to unshift.
@@ -1617,7 +1589,6 @@
     }
     return translist;
   }
-  
 
   Node.prototype.lower = T.makePass("lower", "lowerNode");
 
@@ -1632,29 +1603,12 @@
 
     return this;
   };
-  
+
   FunctionExpression.prototype.lower =
   FunctionDeclaration.prototype.lower = function (o) {
-    var memcheckName;
     o = extend(o);
     o.scope = this.frame;
 
-    if(o.memcheck) {
-      if(this.id && this.id.name) {
-        memcheckName = this.id.name;
-      } else {
-        memcheckName = "<anonymous>";
-      }
-      this.frame.memcheckFnLoc = {name: memcheckName, line: this.loc.start.line, column: this.loc.start.column};
-      this.body.body.unshift(new ExpressionStatement(new CallExpression(this.frame.MEMCHECK_CALL_PUSH(), 
-                                                                        [new Literal(memcheckName),
-                                                                         new Literal(this.loc.start.line),
-                                                                         new Literal(this.loc.start.column)])));
-      
-      if(this.body.body[this.body.body.length-1].type !== 'ReturnStatement') {
-        this.body.body.push(new ExpressionStatement(new CallExpression(this.frame.MEMCHECK_CALL_POP(), [])));
-      }
-    }
     this.body.body = lowerList(this.body.body, o);
     var prologue = createPrologue(this, o);
     var epilogue = createEpilogue(this, o);
@@ -1676,28 +1630,6 @@
     this.body = lowerList(this.body, o);
     return this;
   };
-  
-  function findParentFun(scope) {
-    var name;
-    while(scope.parent) {
-      if(scope.name.indexOf("Function") === 0) {
-        name = scope.name.split(" ")[1];
-      }
-    }
-  }
-  
-  CatchClause.prototype.lower = function(o) {
-    o = extend(o);
-    if(o.memcheck) {
-      var fnLoc = o.scope.frame.memcheckFnLoc;
-      this.body.body.unshift(new ExpressionStatement(new CallExpression(o.scope.frame.MEMCHECK_CALL_RESET(), 
-                                                                        [new Literal(fnLoc.name), 
-                                                                         new Literal(fnLoc.line), 
-                                                                         new Literal(fnLoc.column)])));
-    }
-    return Node.prototype.lower.call(this, o);
-  };
-
 
   Identifier.prototype.lowerNode = function (o) {
     var variable = this.variable;
@@ -1726,21 +1658,13 @@
   ReturnStatement.prototype.lowerNode = function (o) {
     var scope = o.scope;
     var frameSize = scope.frame.frameSizeInWords;
-    if (frameSize || o.memcheck) {
+    if (frameSize) {
       var arg = this.argument;
-      var t = scope.freshTemp(arg.ty, arg.loc);
-      var assn = new AssignmentExpression(t, "=", arg, arg.loc);
-      var exprList = [assn];
-      if(frameSize) {
-        var restoreStack = new AssignmentExpression(scope.frame.realSP(), "+=", new Literal(frameSize));
-        exprList.push(restoreStack);
-      }
-      if(o.memcheck) {
-        var popMemcheck = new CallExpression(scope.MEMCHECK_CALL_POP(), []);
-        exprList.push(popMemcheck);
-      }
-      exprList.push(t);
-      this.argument = new SequenceExpression(exprList, arg.loc);
+      var t = scope.freshTemp(ty, arg.loc);
+      var ref = scope.cacheReference(arg);
+      var assn = new AssignmentExpression(t, "=", ref.def, arg.loc);
+      var restoreStack = new AssignmentExpression(scope.frame.realSP(), "+=", new Literal(frameSize));
+      this.argument = new SequenceExpression([assn, restoreStack, t], arg.loc);
     }
   };
 
@@ -1758,7 +1682,7 @@
         }
       }
     }
-  };
+  }
 
   UnaryExpression.prototype.lowerNode = function (o) {
     var arg = this.argument;
@@ -1796,9 +1720,7 @@
     lowered.ty = this.ty;
     return lowered;
   };
-  
 
-  
   /**
    * Driver
    */
@@ -1807,13 +1729,11 @@
     return new CallExpression(new Identifier("require"), [new Literal(name)]);
   }
 
-  function createModule(program, name, bare, loadInstead, memcheck) {
+  function createModule(program, name, bare, loadInstead) {
     var body = [];
     var cachedMEMORY = program.frame.cachedMEMORY;
     if (cachedMEMORY) {
       var mdecl;
-      // todo: causes all files named "memory.ljs" to be compiled with the memory 
-      // var pointing at exports, probably want a better way of doing that...
       if (name === "memory") {
         mdecl = new VariableDeclarator(cachedMEMORY, new Identifier("exports"));
       } else if (loadInstead) {
@@ -1824,20 +1744,13 @@
         mdecl = new VariableDeclarator(cachedMEMORY, createRequire("memory"));
       }
       body.push(new VariableDeclaration("const", [mdecl]));
-      // todo: broken just like above
-      if(name !== "memory") {
-        body.push(new ExpressionStatement(
-          new CallExpression(new MemberExpression(cachedMEMORY, new Identifier("set_memcheck")), 
-                             [new Literal(memcheck)])));
-      }
-      
     }
 
     if (bare) {
       program.body = body.concat(program.body);
       return program;
     }
-    
+
     body = new BlockStatement(body.concat(program.body));
     var mname = name.replace(/[^\w]/g, "_");
     if (mname.match(/^[0-9]/)) {
@@ -1876,7 +1789,7 @@
 
     // Pass 1.
     var types = resolveAndLintTypes(node, clone(builtinTypes));
-    var o = { types: types, name: name, logger: _logger, warn: warningOptions(options), memcheck: options.memcheck };
+    var o = { types: types, name: name, logger: _logger, warn: warningOptions(options) };
     // Pass 2.
     node.scan(o);
     // Pass 3.
@@ -1884,7 +1797,7 @@
     // Pass 4.
     node = node.lower(o);
 
-    return T.flatten(createModule(node, name, options.bare, options["load-instead"], options.memcheck));
+    return T.flatten(createModule(node, name, options.bare, options["load-instead"]));
   }
 
   exports.compile = compile;
